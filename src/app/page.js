5397f5b8adcb2414b6894eb66626ec2f5f021b4a
Bashamega <adambashaahmednaji@gmail.com>
2023-06-15 18:38:54 +0300

    UPDATE - New review

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import {
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
-  FaYoutube,
   FaYoutubeSquare,
   FaGithubSquare,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
-
 export default function Home() {
   var about =
     "I am very passionate about learning something new and challenging things, so I always try to keep growing and improving my skills. I love coding and want to pursue a career in it.";
   var name = "Adam Basha";
   var image_url =
     "https://i0.wp.com/adambashaahmednaji.com/wp-content/uploads/2023/03/295050490_588427452639580_4228596127967383968_n-e1678714008421.jpg";
-    const handleContactClick = () => {
-      const recipient = "recipient@example.com";
-      const subject = "Contact Request";
-      const body = `Name: ${contactName}\nEmail: ${contactEmail}\n\n${contactMessage}`;
-  
-      const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
-        subject
-      )}&body=${encodeURIComponent(body)}`;
-  
-      window.location.href = mailtoLink;
-    };
-  return (
 
+  return (
     <div>
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
@@ -69,13 +55,20 @@ export default function Home() {
             ></Image>
             <div id="icons">
               <ul className="flex justify-between">
-                <a href="https://www.youtube.com/@bashacoder" target="_blank" className="text-white hover:text-teal-500">
-
+                <a
+                  href="https://www.youtube.com/@bashacoder"
+                  target="_blank"
+                  className="text-white hover:text-teal-500"
+                >
                   <li>
                     <FaYoutubeSquare className="w-10 h-10"></FaYoutubeSquare>
                   </li>
                 </a>
-                <a href="https://github.com/Bashamega" target="_blank" className="text-white hover:text-teal-500">
+                <a
+                  href="https://github.com/Bashamega"
+                  target="_blank"
+                  className="text-white hover:text-teal-500"
+                >
                   <li>
                     <FaGithubSquare className="w-10 h-10"></FaGithubSquare>
                   </li>
@@ -113,18 +106,19 @@ export default function Home() {
           </h2>
           <div className="flex flex-wrap justify-center items-center">
             <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2">
                 <a href="https://educpk.github.io/">
                   <h1>Edu CPK</h1>
                   <p>
-                    EduCPK is an educational coding website that provides a simple and
-                    intuitive text editor for learners to practice coding in various
-                    programming languages. It aims to help users improve their coding
-                    skills through hands-on practice and experimentation.
+                    EduCPK is an educational coding website that provides a
+                    simple and intuitive text editor for learners to practice
+                    coding in various programming languages. It aims to help
+                    users improve their coding skills through hands-on practice
+                    and experimentation.
                   </p>
                 </a>
               </div>
-              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2">
                 <a href="https://github.com/Bashamega/eduquiz">
                   <h1>Edu Quiz</h1>
                   <p>A simple quiz app built using HTML, CSS, and JS</p>
@@ -132,19 +126,18 @@ export default function Home() {
               </div>
             </div>
             <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2">
                 <a href="https://microsoftedge.microsoft.com/addons/detail/cpk/okabgcamgefdfhhlpkdbkohiickenfgg">
                   <h1>Edu CPK Extension</h1>
                   <p>
-                  CPK. This extension helps coders run html code on the web.
+                    CPK. This extension helps coders run html code on the web.
                   </p>
                 </a>
               </div>
-              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-full p-6 text-black w-full lg:w-1/2">
                 <a href="https://microsoftedge.microsoft.com/addons/detail/scratch-dark-mode/kcfjdhhbmklaiijndlffnoihjlobacnh">
                   <h1>Scratch dark mode</h1>
-                  <p>This is a dark mode extension for scratch.
-</p>
+                  <p>This is a dark mode extension for scratch.</p>
                 </a>
               </div>
             </div>
@@ -157,45 +150,61 @@ export default function Home() {
           </h2>
           <div className="flex flex-wrap justify-center items-center">
             <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-              <div className="flex-1 bg-white rounded-md p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-md p-6 text-black w-full lg:w-1/2">
                 <h1>Anonymous</h1>
                 <p>
-                Coder_master has been very helpful in me overcoming various coding challenges while working on my personal projects.<br></br> Not only has he been able to address all of my issues, but when asked, he has also been patient enough to explain each step along the way.<br></br> He's consistently responsive by promptly addressing my questions and offering assistance whenever I need it.<br></br>
-
-As a piece of constructive criticism, I would be to encourage more independent problem-solving rather than immediately providing the solution. While I appreciate his willingness to solve the problem directly, I believe it would be more beneficial for my learning if he initially pointed me in the right direction. If I still need further assistance, then he could provide additional support.
-
-Overall, my experience with Coder_master has been positive, and I am  grateful for the invaluable help he has provided me.
+                  Coder_master has been very helpful in me overcoming various
+                  coding challenges while working on my personal projects.
+                  <br></br> Not only has he been able to address all of my
+                  issues, but when asked, he has also been patient enough to
+                  explain each step along the way.<br></br> He's consistently
+                  responsive by promptly addressing my questions and offering
+                  assistance whenever I need it.<br></br>
+                  As a piece of constructive criticism, I would be to encourage
+                  more independent problem-solving rather than immediately
+                  providing the solution. While I appreciate his willingness to
+                  solve the problem directly, I believe it would be more
+                  beneficial for my learning if he initially pointed me in the
+                  right direction. If I still need further assistance, then he
+                  could provide additional support. Overall, my experience with
+                  Coder_master has been positive, and I am grateful for the
+                  invaluable help he has provided me.
                 </p>
               </div>
-              <div className="flex-1 bg-white rounded-md p-6 text-black w-full lg:w-1/2 hover:p-10">
+              <div className="flex-1 bg-white rounded-md p-6 text-black w-full lg:w-1/2 ">
                 <h1>Anonymous</h1>
                 <p>
-                Thank you @coder_master  for creating an email verifier for me, it’s very helpful and I will be using it a bunch. Very quick delivery and very satisfied.
+                  Thank you @coder_master for creating an email verifier for me,
+                  it’s very helpful and I will be using it a bunch. Very quick
+                  delivery and very satisfied.
                 </p>
               </div>
             </div>
           </div>
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+              <div className="flex-1 bg-white rounded-md p-6 text-black w-full lg:w-1/2">
+                <h1>rorza</h1>
+                <p>
+                the bot works well and is working how i asked plus a few added bonuses like the bold texts and stuff
+                </p>
+              </div>
+              
+            </div>
+          </div>
         </section>
         <hr></hr>
-        <section  id="Contact" className=" mt-10 mb-10">
-            <a href="https://adambashaahmednaji.com/contact/">
-              <button
-                className="bg-gradient-to-r from-cyan-500 text- text-white px-4 py-2 border-none rounded-md  w-full"
-                
-              >
-                Contact
-              </button>
-            </a>
-            
-
-        
+        <section id="Contact" className=" mt-10 mb-10">
+          <a href="https://adambashaahmednaji.com/contact/">
+            <button className="bg-gradient-to-r from-cyan-500 text- text-white px-4 py-2 border-none rounded-md  w-full">
+              Contact
+            </button>
+          </a>
         </section>
-        
+
         <footer className="bg-gray-900 text-white py-8 px-4">
           <div className="container mx-auto text-center">
-            <p className="text-sm">
-              &copy; 2023 {name}. All rights reserved.
-            </p>
+            <p className="text-sm">&copy; 2023 {name}. All rights reserved.</p>
             <ul className="flex justify-center mt-4">
               <li className="mx-2">
                 <a
@@ -220,9 +229,7 @@ Overall, my experience with Coder_master has been positive, and I am  grateful f
             </ul>
           </div>
         </footer>
-
       </main>
-      
     </div>
   );
 }
